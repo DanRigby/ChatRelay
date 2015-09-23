6ae3430ca3f502d1e6460ed5ae85ccb52b0d1943
Dan Rigby <dan@danrigby.com>
2015-09-22 21:57:04 -0400

    Move feature ideas to GitHub issue tracker

--- ChatRelay/Program.cs
+++ ChatRelay/Program.cs
@@ -51,25 +51,3 @@ namespace ChatRelay
         }
     }
 }
-
-// TODO: The adapters use a Connect() Disconnect() naming convention,
-// but right now they don't support reconnecting as Disconnect() is treated as more of a Dispose.
-// Should probably change the naming structure or code layout to fix this. Maybe Connect/Open and Dispose?
-// Alternatively, actually support disconnecting and reconnecting though more error handling is required.
-
-// TODO: Relay emotes.
-
-// TODO: Bot commands.
-// Request that the bot tell you who is in the room in another service (reply via DM).
-// Request information on the other services, mainly URL.
-// Tell you how many people are on the other services (status?).
-
-// TODO: Test on Mono.
-
-// TODO: Logging. Would be good to basically have two log providers, rotating file on disk and console output.
-// Ex, replace Console.WriteLine calls with logging calls.
-
-// TODO: Connection monitoring. Heartbeat? IRC has Ping/Pong.
-
-// TODO: Really think through error handling and resiliency in terms of disconnects/reconnects.
-// May have to look for an alternative to the Slack API library we're using as it doesn't handling things like this well.
